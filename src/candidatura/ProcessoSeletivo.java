52be38892264d83435c2b70adbbd9414178c88f7
YanGuimaraesMartins <134331115+YanGuimaraesMartins@users.noreply.github.com>
2024-06-26 14:04:40 -0300

    Add files via upload

---
+++ src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,41 @@
+package candidatura;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+ public static void main(String[] args) {
+	selecaoCandidatos();
+}
+ static void selecaoCandidatos() {
+	 String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MIRELA","DANIELA","JONAS"};
+	 
+	 int candidatosSelecionados = 0;
+	 int candidatosAtual = 0;
+	 double salarioBase=2000.0;
+	 while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length ) {
+		 String candidato = candidatos[candidatosAtual];
+		 double salarioPretendido = valorPretendido();
+		 
+		 System.out.println("O candidato "+candidato+" Solicitou este valor de salário " + salarioPretendido);
+		 if (salarioBase >= salarioPretendido) {
+			 System.out.println("o candidato "+ candidato+" foi selecionado para a vaga.");
+			 candidatosSelecionados++;
+		 }
+		 candidatosAtual++;
+	 }
+ }
+ static double valorPretendido() {
+	 return ThreadLocalRandom.current().nextDouble(1800, 2200);
+ }
+ static void analisarCandidato(double salarioPretendido) {
+	 double salarioBase = 2000.0;
+	 if(salarioBase > salarioPretendido) {
+		 System.out.println("Ligar para o candidato");
+		 
+	 } else if (salarioBase==salarioPretendido) {
+		 System.out.println("ligar para o candidato com contra proposta");
+	 }else {
+		 System.out.println("Aguardando o resultado dos demais candidatos");
+	 }
+	 
+ }
+}
