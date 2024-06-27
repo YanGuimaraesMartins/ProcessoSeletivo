1d26276565bdf1ff68762d97d637317a820cd1c8
YanGuimaraesMartins <134331115+YanGuimaraesMartins@users.noreply.github.com>
2024-06-27 13:55:12 -0300

    Adicionando ligacao ProcessoSeletivo.java

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,41 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
  public static void main(String[] args) {
-	imprimirSelecionados();
+	 String [] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO" };
+	 for(String candidato:candidatos) {
+		 entrandoEmContato(candidato);
+	 }
 }
+ 
+ static void entrandoEmContato(String candidato) {
+	 	int tentativasRealizadas=1;
+	 	boolean continuarTentando = true;
+	 	boolean atendeu = false;
+	 	
+	 	do {
+	 		atendeu = atender();
+	 		continuarTentando = !atendeu;
+	 		if (continuarTentando)
+	 			tentativasRealizadas++;
+	 		else
+	 			System.out.println("Contato realizado com sucesso!");
+	 	}while(continuarTentando && tentativasRealizadas < 3); 
+	 		
+	 	if(atendeu)
+	 		System.out.println("Conseguimos contato com "+candidato+" na "+tentativasRealizadas+" tentativa(s)");
+	 	else 
+	 		 System.out.println("Não conseguimos contato com "+candidato+" , número máximo de tentativas atingido!");
+ }
+ 
+	 
+ 
+ 
+ static boolean atender() {
+	 return new Random().nextInt(3)==1;
+ }
   static void imprimirSelecionados() {
 	  String[] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO" };
 	  System.out.println("Imprimindo a lista de candidatos infromando o indice do elemento");
